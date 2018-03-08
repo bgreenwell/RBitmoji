d1e947c15dfe698a9332e653444dc0f8c8c974a0
Brandon Greenwell <greenwell.brandon@gmail.com>
2018-03-08 16:49:08 -0500

    add hex logo

---
+++ tools/RBitmoji-logo.R
@@ -0,0 +1,39 @@
+# Load required packages
+library(ggimage)
+library(ggplot2)
+library(grid)
+library(png)
+
+# Load vip image
+img <- rasterGrob(readPNG("tools/drop-mic.png"),
+                  interpolate = TRUE, width = 0.75)
+
+# Hexagon data
+hex <- data.frame(x = 1.35 * 1 * c(-sqrt(3) / 2, 0, rep(sqrt(3) / 2, 2), 0,
+                                   rep(-sqrt(3) / 2, 2)),
+                  y = 1.35 * 1 * c(0.5, 1, 0.5, -0.5, -1, -0.5, 0.5))
+
+# Hexagon logo
+g <- ggplot() +
+  geom_polygon(data = hex, aes(x, y), color = "black",
+               fill = 'white', size = 4) +
+  annotation_custom(img, xmin = -Inf, xmax = Inf, ymin = -0.15, ymax = 1) +
+  annotate(geom = "text", x = 0, y = 0.65, color = "black", size = 8,
+           label = "RBitmoji") +
+  coord_equal(xlim = range(hex$x), ylim = range(hex$y)) +
+  scale_x_continuous(expand = c(0.04, 0)) +
+  scale_y_reverse(expand = c(0.04, 0)) +
+  theme_void() +
+  theme_transparent() +
+  theme(axis.ticks.length = unit(0, "mm"))
+print(g)
+
+png("tools/RBitmoji-logo.png", width = 181, height = 209,
+    bg = "transparent", type = "cairo-png")
+print(g)
+dev.off()
+
+svg("tools/RBitmoji-logo.svg", width = 181 / 72, height = 209 / 72,
+    bg = "transparent")
+print(g)
+dev.off()
