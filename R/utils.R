a629881d4fce8f15f494e07a41af2469ee43b16b
bgreenwell <greenwell.brandon@gmail.com>
2018-05-20 12:12:09 -0400

    paste -v1 automatically

--- R/utils.R
+++ R/utils.R
@@ -71,6 +71,7 @@ get_comic <- function(id, tag) {
 #' @export
 #'
 #' @examples
+#' # ID obtained using get_id()
 #' plot_comic("8b06e67b-d4e9-4f11-a355-f1236df17079-v1", tag = "edvard")
 plot_comic <- function(id, tag) {
   img <- get_comic(id, tag)
@@ -79,45 +80,51 @@ plot_comic <- function(id, tag) {
   plot(as.raster(img))
 }
 
-#' Get a User Id
+#' Get user id
 #'
-#' Get an \code{id} for consumption by \code{\link{plot_comic}} and \code{\link{get_comic}}
+#' Get unique user id for use with \code{\link{plot_comic}} and
+#' \code{\link{get_comic}}.
 #'
-#' @param user_email Character string specifying the email associated with the Bitmoji account
-#' for which you'd like to get/plot comics (e.g., \code{username@@gmail.com}).
+#' @param user_email Character string specifying the email associated with the
+#' Bitmoji account for which you'd like to get/plot comics (e.g.,
+#' \code{username@@gmail.com}).
 #'
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #' id <- get_id("username@@gmail.com")
-#' plot_comic(paste0(id,"-v1"), tag = "edvard")
+#' plot_comic(id, tag = "edvard")
 #' }
 get_id <- function(user_email) {
-  # attempt to login --------------------------------------------------------
+
+  # Attempt to log in
   login_url = 'https://api.bitmoji.com/user/login'
-  login_response <- httr::POST(url = login_url
-                               , httr::add_headers(
-                                 Accept = "application/json"
-                                 , "Accept-Encoding" = "gzip, deflate, br"
-                                 , "Accept-Language" = "en-US,en;q=0.9"
-                                 , Connection = "keep-alive"
-                                 , "Content-Type" = "application/x-www-form-urlencoded"
-                                 , Host = "api.bitmoji.com"
-                                 , Origin = "https://www.bitmoji.com"
-                                 , Referer = "https://www.bitmoji.com/account_v2/"
-                               )
-                               , body = list(
-                                 client_id = "imoji"
-                                 , username = user_email
-                                 , password = getPass::getPass(msg = "PASS")
-                                 , grant_type = "password"
-                                 , client_secret = "secret"
-                               )
-                               , encode = "form"
+  login_response <- httr::POST(
+    url = login_url,
+    httr::add_headers(
+      Accept = "application/json",
+      "Accept-Encoding" = "gzip, deflate, br",
+      "Accept-Language" = "en-US,en;q=0.9",
+      Connection = "keep-alive",
+      "Content-Type" = "application/x-www-form-urlencoded",
+      Host = "api.bitmoji.com",
+      Origin = "https://www.bitmoji.com",
+      Referer = "https://www.bitmoji.com/account_v2/"
+    ),
+    body = list(
+      client_id = "imoji",
+      username = user_email,
+      password = getPass::getPass(
+        msg = "Enter the password for your Bitmoji account:"
+      ),
+      grant_type = "password",
+      client_secret = "secret"
+    ),
+    encode = "form"
   )
 
-  # if not a successful login, stop with most descriptive message available
+  # If log in not successful login, stop and provide message
   if(login_response$status_code != 200) {
     if("message" %in% names(httr::content(login_response))) {
       stop(httr::content(login_response)$message)
@@ -126,46 +133,51 @@ get_id <- function(user_email) {
     }
   }
 
-  # extract token if successful login
+  # If log in successful, extract token
   token <- httr::content(login_response)$access_token
 
-  # fetch id ----------------------------------------------------------------------
+  # Fetch user ID
   avatar_url <- "https://api.bitmoji.com/user/avatar"
-  avatar_response <- httr::GET(url = avatar_url
-                               , httr::add_headers(
-                                 "Accept-Encoding" = "gzip, deflate, br"
-                                 , "Accept-Language" = "en-US,en;q=0.9"
-                                 , "bitmoji-token" = token
-                                 , Connection = "keep-alive"
-                                 , "Content-Type" = "application/x-www-form-urlencoded"
-                                 , Host = "api.bitmoji.com"
-                                 , Origin = "https://www.bitmoji.com"
-                                 , Referer = "https://www.bitmoji.com/account_v2/"
-                               )
+  avatar_response <- httr::GET(
+    url = avatar_url,
+    httr::add_headers(
+      "Accept-Encoding" = "gzip, deflate, br",
+      "Accept-Language" = "en-US,en;q=0.9",
+      "bitmoji-token" = token,
+      Connection = "keep-alive",
+      "Content-Type" = "application/x-www-form-urlencoded",
+      Host = "api.bitmoji.com",
+      Origin = "https://www.bitmoji.com",
+      Referer = "https://www.bitmoji.com/account_v2/"
+    )
   )
-  # stop if request fails
+
+  # Stop if request fails
   if(avatar_response$status_code != 200) {
-    stop(httr::content(login_response)$error)
+    stop(httr::content(login_response)$error, call. = FALSE)
   }
-  # extract id if successful avatar_response
-  uuid <- httr::content(avatar_response)$avatar_version_uuid
 
-  # attempt to logout -------------------------------------------------------
+  # extract id if successful avatar_response
+  uid <- httr::content(avatar_response)$avatar_version_uuid
+
+  # Attempt to log out
   logout_url <- "https://api.bitmoji.com/user/logout"
-  logout_response <- httr::POST(url = logout_url
-                                , httr::add_headers(
-                                  Accept = "application/json"
-                                  , "Accept-Encoding" = "gzip, deflate, br"
-                                  , "Accept-Language" = "en-US,en;q=0.9"
-                                  , "bitmoji-token" = token
-                                  , Connection = "keep-alive"
-                                  , "Content-Type" = "application/x-www-form-urlencoded"
-                                  , Host = "api.bitmoji.com"
-                                  , Origin = "https://www.bitmoji.com"
-                                  , Referer = "https://www.bitmoji.com/account_v2/"
-                                )
+  logout_response <- httr::POST(
+    url = logout_url,
+    httr::add_headers(
+      Accept = "application/json",
+      "Accept-Encoding" = "gzip, deflate, br",
+      "Accept-Language" = "en-US,en;q=0.9",
+      "bitmoji-token" = token,
+      Connection = "keep-alive",
+      "Content-Type" = "application/x-www-form-urlencoded",
+      Host = "api.bitmoji.com",
+      Origin = "https://www.bitmoji.com",
+      Referer = "https://www.bitmoji.com/account_v2/"
+    )
   )
 
-  # return response ---------------------------------------------------------
-  return(uuid)
+  # Return user ID
+  paste0(uid, "-v1")  # FIXME: Is this always needed?
+
 }
